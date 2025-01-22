4da3e3247b4ee2629ec7e2045ecca8f6e966f17f
Ruben Salazar <rubensalazar@rocketmail.com>
2025-01-21 18:16:29 -0600

    email parser lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+    
+    def parse(self):
+        
+        email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+        
+        
+        addresses = re.split(r'[,\s]+', self.email_addresses.strip())
+        
+        
+        valid_addresses = [email for email in addresses if re.match(email_pattern, email)]
+        
+        unique_addresses = sorted(set(valid_addresses))
+        
+        return unique_addresses
